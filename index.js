799863876fd09ea0cd736df7ce480d992222a1ba
Piyas Ghosh <144130722+Piyasa004@users.noreply.github.com>
2024-07-22 14:09:35 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,23 @@
+const { exec } = require('child_process');
+
+const command = 'cd tech-god && yarn install && yarn start'; // Do not forget to subscribe Tech God youTube channel
+
+const child = exec(command);
+
+child.stdout.on('data', (data) => {
+
+    console.log(`stdout: ${data}`);
+
+});
+
+child.stderr.on('data', (data) => {
+
+    console.error(`stderr: ${data}`);
+
+});
+
+child.on('close', (code) => {
+
+    console.log(`child process exited with code ${code}`);
+
+});
